9e554d3238ad5bf5cbd1b9b493ff469d4c983de4
Ashraful Mahin <mahinashraful08@gmail.com>
2025-03-01 11:06:25 -0500

    Initial Commit

---
+++ Travel_Agent/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Travel_Agent
+//
+//  Created by Mahin Ashraful on 3/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Travel_Agent/Travel_AgentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Travel_AgentApp.swift
+//  Travel_Agent
+//
+//  Created by Mahin Ashraful on 3/1/25.
+//
+
+import SwiftUI
+
+@main
+struct Travel_AgentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
